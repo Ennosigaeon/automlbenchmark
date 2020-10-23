b64edaf816ac4d52223290762ec9c93e2a8cb188
Marc Zoeller <m.zoeller@usu.de>
2020-10-23 17:13:19 +0200

    Changes for local setup

--- frameworks/dswizard/setup.sh
+++ frameworks/dswizard/setup.sh
@@ -11,13 +11,13 @@ rm -Rf ${TARGET_DIR}
 mkdir ${HERE}/lib
 
 # sudo apt install graphviz swig libgraphviz-dev
-# ln -s ~/phd/code/pynisher ${TARGET_DIR}/pynisher
-# ln -s ~/phd/code/sklearn-components ${TARGET_DIR}/sklearn-components
-# ln -s ~/phd/code/dswizard ${TARGET_DIR}/dswizard
+ln -s ~/phd/code/pynisher ${TARGET_DIR}/pynisher
+ln -s ~/phd/code/sklearn-components ${TARGET_DIR}/sklearn-components
+ln -s ~/phd/code/dswizard ${TARGET_DIR}/dswizard
 
-git clone --depth 1 --single-branch --branch master --recurse-submodules git@github.com:Ennosigaeon/pynisher.git ${TARGET_DIR}/pynisher
-git clone --depth 1 --single-branch --branch master --recurse-submodules git@github.com:Ennosigaeon/sklearn-components.git ${TARGET_DIR}/sklearn-components
-git clone --depth 1 --single-branch --branch master --recurse-submodules git@github.com:Ennosigaeon/dswizzard.git ${TARGET_DIR}/dswizard
+# git clone --depth 1 --single-branch --branch master --recurse-submodules git@github.com:Ennosigaeon/pynisher.git ${TARGET_DIR}/pynisher
+# git clone --depth 1 --single-branch --branch master --recurse-submodules git@github.com:Ennosigaeon/sklearn-components.git ${TARGET_DIR}/sklearn-components
+# git clone --depth 1 --single-branch --branch master --recurse-submodules git@github.com:Ennosigaeon/dswizzard.git ${TARGET_DIR}/dswizard
 
 PIP install -e ${TARGET_DIR}/pynisher
 PIP install -e ${TARGET_DIR}/sklearn-components
